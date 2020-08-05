4cae67c3a3dd1956442d54382174d6de680263f9
SAYAHNA  SENGUPTA <46857789+sayahna22@users.noreply.github.com>
2020-08-05 11:58:00 +0530

    Create String_Slice_Indexing.py

---
+++ String_Slice_Indexing.py
@@ -0,0 +1,11 @@
+name="Sayahna Sengupta"
+#print last index
+print(name[-1])
+#index Slicing
+print(name[0:4])
+print(name[4:])
+print(name[:5])
+#print the length of the string
+print(len(name))
+#note:String Is Immutable We cant  change the value of string
+print('my'+name[4:])
